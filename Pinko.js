64662ea5132f8a10b9cd90543c5d0eac265f659a
Arnav-Mandhare <69675421+Arnav-Mandhare@users.noreply.github.com>
2020-09-12 14:12:20 +0530

    Add files via upload

---
+++ Pinko.js
@@ -0,0 +1,34 @@
+class Pinko{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:true,
+			restitution:0.3,
+			friction:0.5,
+			density:1.2
+
+			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var pinkopos=this.body.position;		
+
+			push()
+			translate(pinkopos.x, pinkopos.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			fill(255)
+			ellipse(0,0,this.r, this.r);
+			pop()
+			
+	}
+
+}
